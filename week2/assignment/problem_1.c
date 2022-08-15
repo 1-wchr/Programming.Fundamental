a9937c8cf8d157e775db0303ce70995824461e80
Watcharin Damthong <95212845+1-wchr@users.noreply.github.com>
2022-08-15 22:32:01 +0700

    Add files via upload

---
+++ week2/assignment/problem_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, result;
+    printf("Enter number1: ");
+    scanf("%d", &n1);
+    printf("Enter number2: ");
+    scanf("%d", &n2);
+
+    if (result = n1 + n2)
+    {
+        printf("ans1 = %d\n", result);
+    }
+    if (result = n1 - n2)
+    {
+        printf("ans2 = %d\n", result);
+    }
+    if (result = n1 * n2)
+    {
+        printf("ans3 = %d\n", result);
+    }
+    if (result = n1 / n2)
+    {
+        printf("ans4 = %d\n", result);
+    }
+
+    return 0;
+}
